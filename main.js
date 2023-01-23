ea5b344697e8bb71963ade46979794f53bbb17d4
Kalina Petersen <petersenkalina@gmail.com>
2023-01-23 11:35:33 -0800

    Add functionality to update and display round and game results

--- main.js
+++ main.js
@@ -2,32 +2,23 @@ const choices = ['rock','paper','scissors']
 let playerScore = 0;
 let computerScore = 0;
 
-const buttons = document.querySelectorAll('.js-button')
+const buttons = document.querySelectorAll('.js-button');
+const roundInfo = document.querySelector('.roundInfo');
+const roundResults = document.querySelector('.roundResults');
+const gameInfo = document.querySelector('.gameInfo');
+const gameOver = document.querySelector('.gameOver');
 
 buttons.forEach((button) => {
     button.addEventListener('click',(e) => {
         getPlayerChoice(e);
+        game();
     });
 });
 
 function getPlayerChoice (event){
     console.log(event);
     const input = event.target.value;
-    // let input = prompt('Choose rock, paper, or scissors');
-    //while (input == null){  
-    //    input = prompt('Choose rock, paper, or scissors');
-    //}
-    //input = input.toLowerCase();
-    //let check = validateInput(input);
-    // while (check == false){
-    //     input = prompt('Please choose rock, paper, or scissors');
-    // while (input == null) {
-    //     input = prompt('Choose rock, paper, or scissors');
-    // }
-    
-    //     input = input.toLowerCase();
-    //     check = validateInput(input);
-    //}
+
     playRound(input, computerChoice);
 }        
 
@@ -35,30 +26,36 @@ function computerChoice(){
     return choices[Math.floor(Math.random()*3)];
 }
 
-function validateInput(choice){
-    return choices.includes(choice);
-}
-
 function playRound(playerSelection){
     const computerSelection = computerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
+    roundInfo.textContent = `The computer chose ${computerSelection}.`;
 
     if (playerSelection === computerSelection) {
-        console.log('The result is a tie!');
+        roundResults.textContent = 'The result is a tie!';
     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') 
         || (playerSelection === 'paper' && computerSelection === 'rock') 
         || (playerSelection === 'scissors' && computerSelection === 'paper')) 
     {
         playerScore += 1;
-        console.log('Player wins round!');
+        roundResults.textContent = 'You win this round: ' + playerSelection + ' beats ' + 
+        computerSelection + '!';
         }
     else {
         computerScore += 1;
-        console.log('Computer wins round!');
+        roundResults.textContent = 'You lose this round: ' + computerSelection + ' beats ' + 
+        playerSelection + '!';
 
     }
 }
+
+function game() {
+    gameInfo.textContent = `Player - ${playerScore} Computer - ${computerScore}`;
+    if (playerScore === 5)
+    gameOver.textContent = 'You won the game!';
+    else if (computerScore === 5)
+    gameOver.textContent = 'The computer won the game!';
+}
+
 
 //function init() {
 //    for(let i = 0; i <= 5; i++){
